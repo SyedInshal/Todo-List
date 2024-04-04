44cc72db999c668b5b22d136a2a4bd23eebaeea8
Syed Inshal <160940437+SyedInshal@users.noreply.github.com>
2024-04-05 04:19:27 +0500

    Pushing The Code To Github

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let todos = []
+let condition = true;
+while(condition)
+{
+let todosQuestions = await inquirer.prompt(
+[
+        {
+    
+        name:"firstQuestion",
+        type:"input",
+        message:"what would you like to add in you todos?"
+
+    },
+    {
+        name:"secondQuestion",
+        type:"confirm",
+        message:"what would you like to add more in your todos?",
+        default:"true",
+    }
+]
+);
+
+todos.push(todosQuestions.firstQuestion);
+console.log(todos);
+condition = todosQuestions.secondQuestion;
+
+}
